04c141fbac5f8c50b492ca4a6a35195486487fcd
juune622 <juune622@naver.com>
2021-02-26 15:29:26 +0900

    ...

---
+++ views/layouts/default.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  div.container-wrapper
+    header.header-wrapper
+      h1 Header
+    Nuxt.nuxt-wrapper
+    footer.footer-wrapper
+      p Copyright Juune
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss">
+  .container-wrapper{
+    height: 100vh;
+    @include flex($FS,$ST);
+    flex-direction: column;
+    .header-wrapper{
+      background-color: $colorBlack;
+      color: #FFF;
+    }
+    .nuxt-wrapper{
+      flex-grow:1;
+    }
+    .footer-wrapper{
+      background-color: $colorBlack;
+      color: #FFF;
+    }
+  }
+</style>
